a6e57e6cd24eefd533d63958f7bfae3e4179c0e8
bismawahyueka <bismaepu@gmail.com>
2025-05-06 14:08:30 +0700

    revisi

--- admin/e_kategori.php
+++ admin/e_kategori.php
@@ -67,7 +67,7 @@ if (isset($_POST['simpan'])) {
 
     
   <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
         <span class="d-none d-lg-block">RupaRupa</span>
       </a>
